4e8b0c825010d5f46b668caa7d9baa3d0f2f8433
uNerMar <uNerMar@outlook.com>
2020-12-02 15:34:43 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello KLOWN World!");
+            int a = 1;
+            int b = 2;
+            Console.WriteLine(a+b);
+        }
+    }
+}
